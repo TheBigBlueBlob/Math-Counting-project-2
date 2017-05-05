75567e9b7041591b7d56147b031e4a83bccf2e3d
TheBigBlueBlob <liamszczepiorkowski@hanovernorwichschools.org>
2017-05-05 11:37:39 -0400

    Updated from Brython Server: 5/5/2017 11:37:38 AM

--- practice.py
+++ practice.py
@@ -1,1 +1,17 @@
+from random import shuffle, randint
 
+trees = ['M','M','M','O','O','O','O','B','B','B','B','B']
+
+failures = 0
+trials = 10
+
+for i in range(trials):
+    
+    shuffle(trees)
+    print(trees)
+    for j in range(9):
+        if trees[j] + trees[j+1] + trees[j+2] + trees[j+3] == 'BBBB':
+            failures+=1
+            break
+        
+print ((trials-failures)/trials*100, '%')
